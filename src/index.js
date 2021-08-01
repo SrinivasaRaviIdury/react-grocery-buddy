7c34f75fc3950a17a2f212d2338658eeedb2e4d3
SrinivasaRaviAhlad <42990095+raviaahlad@users.noreply.github.com>
2021-08-01 22:08:20 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <p style={{ color: "red" }}>Thanks and Credit to JohnSmiliga</p>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
